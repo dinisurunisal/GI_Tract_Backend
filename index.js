57df3582071e298c2b244d0eb7a0ae8907374024
Dinisuru.2018559@outlook.com <dinisurunisal@gmail.com>
2022-04-06 22:16:34 +0530

    upload file updated

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const axios = require('axios');
-const fileupload = require('express-fileupload');  
+const fs = require('fs');
+const uploadFile = require("./middleware/upload");
 
 const app = express();
 const port = 3000;
 
+global.__basedir = __dirname;
+
+app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
@@ -16,7 +20,7 @@ app.get('/', function(req, res) {
     res.send('GET request to homepage')
 })
 
-app.get('/home', async function(req, res){
+app.get('/api/home', async function(req, res){
     console.log('calling');
 
     const predict = await axios({
@@ -33,11 +37,45 @@ app.get('/home', async function(req, res){
     }
 });
 
-app.post('/upload', function(req, res) {
-    let file = req['files'].images;  
-  
-    console.log("File uploaded: ", file.name);  
+app.post('/api/upload', async function(req, res) {
+    try {
+        await uploadFile(req, res);
+        if (req.file == undefined) {
+          return res.status(400).send({ message: "Please upload a file!" });
+        }
+        res.status(200).send({
+          message: "Uploaded the file successfully: " + req.file.originalname,
+        });
+    } catch (err) {
+        if (err.code == "LIMIT_FILE_SIZE") {
+            return res.status(500).send({
+                message: "File size cannot be larger than 2MB!",
+            });
+    }
+    res.status(500).send({
+        message: `Could not upload the file: ${req.file.originalname}. ${err}`,
+    });
+    }  
 })
+
+app.get('/api/getfiles', function(req, res){
+    const directoryPath = __basedir + "/assets/uploads/";
+    fs.readdir(directoryPath, function (err, files) {
+      if (err) {
+        res.status(500).send({
+          message: "Unable to scan files!",
+        });
+      }
+      let fileInfos = [];
+      files.forEach((file) => {
+        fileInfos.push({
+          name: file,
+          url: 'http://localhost:3000/api/getfiles/' + file,
+        });
+      });
+      res.status(200).send(fileInfos);
+    });
+}) 
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
